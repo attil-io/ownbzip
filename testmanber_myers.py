d82f188b0276bea0d6fed9c0d2b35c596b4ade2f
Attilio <parrotchaffinch@gmail.com>
2016-11-12 18:10:05 +0100

    Empty test for Manber-Myers.

---
+++ testmanber_myers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from manber_myers import *
+import unittest
+
+
+class MMTest(unittest.TestCase):
+    def testDummy(self):
+        self.assertEqual(1, 2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
