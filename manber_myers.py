0dc6e73c9986e9702a140c044058a611b0db7db9
Attilio <parrotchaffinch@gmail.com>
2016-11-12 18:46:10 +0100

    Simplify algorithm (remove matrix, unnecessary variables).

--- manber_myers.py
+++ manber_myers.py
@@ -8,29 +8,21 @@ def manberMyers(word):
 	       return -1
 
 	n=len(word)
-	P=[]
-	P0 = []
+	Pprev = []
 	sortedword = sorted(set(word))
 	for i in range(n):
-	    P0.append(sortedword.index(word[i])) 
+	    Pprev.append(sortedword.index(word[i])) 
 
-	P.append(P0)
-
-	cnt = 1
-        lengths = [2**x for x in range(n) if 2**(x-1) < n]
-	for ind,cnt in enumerate(lengths):
-            k=ind + 1
+	for cnt in [2**x for x in range(n) if 2**(x-1) < n]:
 	    L=[]
 	    for i in range(n):
-		L.append((P[k-1][i], safeGetIdx(P[k-1], i + cnt), i))
+		L.append((Pprev[i], safeGetIdx(Pprev, i + cnt), i))
 	    sortedL = sorted(L, key=lambda x: (x[0], x[1]))
-	    Pk = [0] * n
 	    for i in range(n):
 		if i > 0 and sortedL[i][0] == sortedL[i - 1][0] and sortedL[i][1] == sortedL[i - 1][1]:
-		   Pk[sortedL[i][2]] = Pk[sortedL[i-1][2]]
+		   Pprev[sortedL[i][2]] = Pprev[sortedL[i-1][2]]
 		else:
-		   Pk[sortedL[i][2]] = i
-	    P.append(Pk)
-	return P[-1]
+		   Pprev[sortedL[i][2]] = i
+	return Pprev
 
 
